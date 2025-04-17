3ec6840ba354b5d2f5cb69f5c00b03ae04f0f358
WindroseFS <almir.junior445@gmail.com>
2025-04-17 20:25:14 -0300

    commit 2

--- Projeto/kowalski/views.py
+++ Projeto/kowalski/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+from .models import Todo
+
+class TodoListView(ListView):
+    model = Todo
